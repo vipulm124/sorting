74441f7f5e3b7dd66fa0b313935436d1dd7ba9b2
Vipul Malhotra <vipulm124@hotmail.com>
2022-07-12 15:32:50 +0530

    Searching

---
+++ binarySearchIterative.js
@@ -0,0 +1,27 @@
+function binarySearch(arr, val) {
+    
+    let low = 0;
+    let high = arr.length;
+    
+
+    while (low <= high) {
+        mid = parseInt(low + (high - low) / 2);
+        //element is on left
+        if (val < arr[mid]) {
+            high = mid - 1;
+        }
+
+        //element is on right
+        else if (val > arr[mid]) {
+            low = mid + 1;
+        }
+        else {
+        return mid;
+        }
+    }
+    return -1;
+}
+
+let arr = [-12,-9,-2,11,45];
+let index = binarySearch(arr, -92);
+console.log(index);
