d5799f751a397d4d0be60ffd18eadd5295101bcb
Vipul Malhotra <vipulm124@hotmail.com>
2022-07-12 15:23:26 +0530

    change in condition

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,7 +3,7 @@ function bubbleSort(arr) {
     let len = arr.length;
 
     for (i = 0; i < len - 1; i++){
-        for (j = 0; j < len - 1; j++){
+        for (j = 0; j < len -i- 1; j++){
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
